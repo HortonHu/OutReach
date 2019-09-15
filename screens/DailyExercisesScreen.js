8ed142f3cf81951db1b6e99ff5ebb6c11db21765
horton <wcbieyuan@gmail.com>
2019-09-14 17:27:00 -0700

    add reps input

--- screens/DailyExercisesScreen.js
+++ screens/DailyExercisesScreen.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { StyleSheet, Button, View, Image, Text } from 'react-native';
+import { StyleSheet, Button, View, TextInput, Text } from 'react-native';
 import { Video } from 'expo-av';
 
 
 export default class DailyExercisesScreen extends React.Component {
-  componentDidMount() {
-    console.log("Go to Daily Exercises Screen.");
-  }
+  state = {
+    set1_num: '',
+    set2_num: '',
+    set3_num: '',
+  };
 
   render() {
     return (
-      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <View style={styles.container}>
         <Text>Daily Exercises Screen</Text>
 
         <Video
@@ -21,14 +23,87 @@ export default class DailyExercisesScreen extends React.Component {
           resizeMode="cover"
           shouldPlay
           isLooping
-          style={{width: 300, height: 300}}
+          style={styles.video}
         />
+
+        <View style={styles.setsContainer}>
+          <View style={styles.setRecordContainer}>
+            <Text style={styles.text}>Set1 Reps Completed</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="0"
+              keyboardType='numeric'
+              onChangeText={
+                (set1_num) => this.setState({set1_num})
+              }
+              value={this.state.set1_num}
+            />
+            </View>
+
+          <View style={styles.setRecordContainer}>
+            <Text style={styles.text}>Set2 Reps Completed</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="0"
+              keyboardType='numeric'
+              onChangeText={
+                (set2_num) => this.setState({set2_num})
+              }
+              value={this.state.set2_num}
+            />
+          </View>
+
+          <View style={styles.setRecordContainer}>
+            <Text style={styles.text}>Set3 Reps Completed</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="0"
+              keyboardType='numeric'
+              onChangeText={
+                (set3_num) => this.setState({set3_num})
+              }
+              value={this.state.set3_num}
+            />
+          </View>
+        </View>
+
       </View>
     );
   }
-
 }
 
 DailyExercisesScreen.navigationOptions = {
   title: 'Daily Exercises',
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  setsContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 50,
+  },
+  setRecordContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 10,
+  },
+  video: {
+    width: 300,
+    height: 300,
+  },
+  input: {
+    borderColor: 'black',
+    borderWidth: 1,
+    width: 100,
+    marginLeft: 10,
+    textAlign: 'center',
+  },
+  text: {
+    textAlign: 'center',
+  }
+});
